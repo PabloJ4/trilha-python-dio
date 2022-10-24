2245c98f91e8b01e057ca632ff1b23c70b0c6668
PabloJ4 <008pablo008@gmail.com>
2022-10-24 16:36:57 -0300

    "Atualização do repositorio"

---
+++ listas.py
@@ -0,0 +1,17 @@
+frutas = ["laranja", "maça", "limão", "morango"]
+
+print(frutas[0]) #laranja
+print(frutas[3]) #morango
+
+lista =["p", "y","t", "h", "o", "n"]
+print(lista[2:])
+print(lista[:2])
+print(lista[1:3])
+print(lista[0:3:2])
+print(lista[::])
+print(lista[::-1])
+
+carros =["forde", "savero", "selta", "palio"]
+for carro in carros:
+    print(carro)
+
